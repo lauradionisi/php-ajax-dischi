a21bb0a612da3fa5a65264e06c073f4959e3254c
Laura <laura.dionisi91@gmail.com>
2021-04-28 10:46:46 +0200

    php-ajax-dischi milestone 1

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=XXXXX<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
     <title>Php-dischi</title>
 </head>
 <body>
@@ -13,7 +14,7 @@
     <div class="wrapper">
         <header>
             <div class="logo">
-                <img src="img/logo.svg" alt="">
+                <i class="fab fa-spotify"></i>
             </div>
         </header>
         <div class="card-container">
@@ -21,12 +22,18 @@
                 foreach ($database as $data) { ?>
               
                     <div class="card">
-                        <?php echo '<img src="'. $data['poster'].'" />' ?>
-                        <?php echo $data['title']; ?>
-                        <?php echo '<br>'; ?>
-                        <?php echo $data['author']; ?>
-                        <?php echo '<br>'; ?>
-                        <?php echo $data['year']; ?>           
+                        <div class="card-img">
+                            <?php echo '<img src="'. $data['poster'].'" />' ?>
+                        </div>
+                        
+                        <div class="card-info">
+                            <h3><?php echo $data['title']; ?></h3> 
+                            <?php echo '<br>'; ?>
+                            <p><?php echo $data['author']; ?></p>
+                            <?php echo '<br>'; ?>
+                            <h4><?php echo $data['year']; ?></h4>
+                        </div>
+                                
                     </div>
                              
             <?php } ?>          
